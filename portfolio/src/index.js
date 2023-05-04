2401826e9e3fa5944e172d3257c01766db43d3ef
Precious Gartrell <precious.gartrell@allovue.com>
2023-05-04 10:14:05 -0400

    Installed Bootstrap and Navbar

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Title from './components/Title';
+import NavBar from './components/NavBar';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavBar />
+    <Title />
   </React.StrictMode>
 );
 
