674d3708d5b1db501c7a25510fd734aff1e8fcbd
Precious Gartrell <precious.gartrell@allovue.com>
2023-05-15 13:04:17 -0400

    installed Tailwind

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -6,15 +6,21 @@ import './App.css';
 
 function NavBar() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar className='bg-indigo-500' bg="light" expand="lg">
         <Container>
             <Navbar.Brand href="#home">I Code Things</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav>
-                    <Nav.Link href="#home">About Me</Nav.Link>
+            <Navbar.Collapse className="justify-content-end"id="basic-navbar-nav">
+              {/* <Nav className="justify-content-center">                  
+                  <Nav.Link  href="#home">About Me</Nav.Link>               
+              </Nav> */}
+              <li className='text-center'><a href="#">About Me</a></li>
+
+                <Nav className="mainNav">
+                  <div className='flex'>
                     <Nav.Link href="#link">Projects</Nav.Link>
                     <Nav.Link href="#link">Contact Me</Nav.Link>
+                  </div>
                 </Nav>
             </Navbar.Collapse>
         </Container>
