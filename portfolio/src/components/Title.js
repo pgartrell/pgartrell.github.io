da3a076efc46481342a671f7736a2c07d0366cde
Precious Gartrell <precious.gartrell@allovue.com>
2023-06-07 08:31:01 -0400

    test

--- portfolio/src/components/Title.js
+++ portfolio/src/components/Title.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function Title() {
   return (
-    <div className="App">
+    <div className="App inline-block m-10">
       <h1 className="title">Hi there! </h1>
       <h2 className="titleName">I'm Precious</h2>
       <h2 className="subtitleName">(Yes that's my name)</h2>
