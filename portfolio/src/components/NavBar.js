2866299752bf96b1371b0000776192c0d1d0d5ad
Precious Gartrell <precious.gartrell@allovue.com>
2023-06-12 12:19:59 -0400

    reaaded tailwind css and added font aweome

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import './App.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faBars} from "@fortawesome/free-solid-svg-icons"
 
 function NavBar() {
   return (
-    <nav className="bg-slate-50" expand="lg">
-        
+    <nav className="flex bg-slate-50" expand="lg">
+          
             <div className="logo" href="#home">I Code Things</div>
-
-                <div >
+             
+                <div>
                     <li className="about_me_li" > <a href="#aboutMe"/>About Me</li>
                 </div>
                 <div className="right_side_nav">
                     <li className="projects_li"> <a href="#projects"/>Projects</li>
                     <li className="contact_me_li"> <a href="#contactMe"/>Contact Me</li>
                 </div>
-            
-        
+              
+                <input type="checkbox" id="checkbox_toggle" />
+                <label class="hamburger hidden select-none" for="checkbox_toggle"><FontAwesomeIcon icon={faBars}></FontAwesomeIcon></label>
+         
     </nav>
 
   
